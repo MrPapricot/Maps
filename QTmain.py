b25f9c3b3370947a145035402a0681270f0a8e4a
RomanPivovaroff <romain.pivovaroff@yandex.ru>
2023-02-03 19:04:32 +0700

    pygame => pyqt

--- QTmain.py
+++ QTmain.py
@@ -10,6 +10,11 @@ class MyWidget(QMainWindow):
     def __init__(self, size=10, x=60, y=60, delta=0.821, mod='map'):
         super().__init__()
         uic.loadUi('qtinterface.ui', self)  # Загружаем дизайн
+        self.pushButton_2.clicked.connect(self.type_map)
+        self.pushButton_3.clicked.connect(self.type_map)
+        self.pushButton_4.clicked.connect(self.type_map)
+
+
         self.size = size
         self.x, self.y = x, y
         self.delta = delta
@@ -18,6 +23,19 @@ class MyWidget(QMainWindow):
 
     def get_zapros(self):
         self.label.setPixmap(QPixmap(qtfunc.createMap(self.x, self.y, self.size, self.mod)))
+
+    def type_map(self):
+        bt = self.sender()
+        dict = {
+            'Карта': 'map',
+            'Спутник': 'sat',
+            'Гибрид': 'sat,skl'
+        }
+        n = bt.text()
+        self.mod = dict[n]
+        self.get_zapros()
+
+
 
 
 if __name__ == '__main__':
