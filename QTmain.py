744385f88b97be1760c3e80dc3eabe82f39d5ffc
Daytly <115785845+Daytly@users.noreply.github.com>
2023-02-03 19:29:54 +0700

    hi

--- QTmain.py
+++ QTmain.py
@@ -10,14 +10,22 @@ class MyWidget(QMainWindow):
     def __init__(self, size=10, x=60, y=60, delta=0.821, mod='map'):
         super().__init__()
         uic.loadUi('qtinterface.ui', self)  # Загружаем дизайн
+        self.map.toggled.connect(self.setMode)
+        self.skl.toggled.connect(self.setMode)
+        self.sat.toggled.connect(self.setMode)
         self.size = size
         self.x, self.y = x, y
         self.delta = delta
         self.mod = mod
-        self.get_zapros()
+        self._get()
 
-    def get_zapros(self):
+    def _get(self):
         self.label.setPixmap(QPixmap(qtfunc.createMap(self.x, self.y, self.size, self.mod)))
+
+    def setMode(self):
+        send = self.sender().text()
+        self.mod = send
+        self._get()
 
 
 if __name__ == '__main__':
