28faf248adf0613c08c165cacff414c91183cccd
RomanPivovaroff <romain.pivovaroff@yandex.ru>
2023-04-30 16:56:55 +0700

    1-12(коммиты в ветке pushbutton)

---
+++ QTmain.py
@@ -0,0 +1,114 @@
+import qtfunc
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+
+class MyWidget(QMainWindow):
+    def __init__(self, size=10, x=60, y=60, delta=0.821, mod='map'):
+        super().__init__()
+        uic.loadUi('qtinterface.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.find_toponim)
+        self.pushButton_2.clicked.connect(self.type_map)
+        self.pushButton_3.clicked.connect(self.type_map)
+        self.pushButton_4.clicked.connect(self.type_map)
+        self.pushButton_5.clicked.connect(self.reset)
+        self.pushButton_6.clicked.connect(self.switch)
+        self.size = size
+        self.x, self.y = x, y
+        self.delta = delta
+        self.mod = mod
+        self.get_zapros()
+
+    def reset(self):
+        self.textBrowser.setText('')
+        qtfunc.pt = ''
+        self.get_zapros()
+
+    def switch(self):
+        qtfunc.flag = not qtfunc.flag
+        self.find_toponim()
+
+
+    def find_toponim(self):
+        text = self.lineEdit.text()
+        a = qtfunc.find_toponim(text)
+        self.textBrowser.setText(a[1])
+        a = a[0].split()
+        self.x = float(a[0])
+        self.y = float(a[1])
+        self.get_zapros()
+
+    def get_zapros(self):
+        self.label.setPixmap(QPixmap(qtfunc.createMap(self.x, self.y, self.size, self.mod)))
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.size += 1
+            self.size %= 18
+            self.delta /= 2
+        elif event.key() == Qt.Key_PageDown:
+            self.size -= 1
+            self.size %= 18
+            self.delta *= 2
+        elif event.key() == Qt.Key_Up or event.key() == Qt.Key_W:
+            if -90 < self.y + self.delta < 90:
+                self.y += self.delta
+        elif event.key() == Qt.Key_Down or event.key() == Qt.Key_S:
+            if -90 < self.y - self.delta < 90:
+                print(123)
+                self.y -= self.delta
+        elif event.key() == Qt.Key_Right or event.key() == Qt.Key_D:
+            if -180 < self.x + self.delta < 180:
+                self.x += self.delta
+        elif event.key() == Qt.Key_Left or event.key() == Qt.Key_A:
+            if -180 < self.x - self.delta < 180:
+                self.x -= self.delta
+        self.get_zapros()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            x, y = (event.x(), event.y())
+            mx, my = self.label.width(), self.label.height()
+            if y <= my:
+                x = self.x + (((x - mx // 2) / mx) * self.delta)
+                y = self.y - (((y - my // 2) / my) * self.delta / 3)
+                a = qtfunc.click_map(x, y, str(self.delta / 2), False)
+                if a:
+                    self.textBrowser.setText(a)
+                else:
+                    self.textBrowser.setText('не удалось ничего найти')
+        elif event.button() == Qt.RightButton:
+            x, y = (event.x(), event.y())
+            mx, my = self.label.width(), self.label.height()
+            if y <= my:
+                x = self.x + (((x - mx // 2) / mx) * self.delta)
+                y = self.y - (((y - my // 2) / my) * self.delta / 3)
+                a = qtfunc.click_map(x, y, str(self.delta / 2), True)
+                if a:
+                    self.textBrowser.setText(a)
+                else:
+                    self.textBrowser.setText('не удалось ничего найти')
+        self.get_zapros()
+
+
+    def type_map(self):
+        bt = self.sender()
+        dict = {
+            'Карта': 'map',
+            'Спутник': 'sat',
+            'Гибрид': 'sat,skl'
+        }
+        n = bt.text()
+        self.mod = dict[n]
+        self.get_zapros()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
---
+++ qtfunc.py
@@ -0,0 +1,98 @@
+import os
+import random
+import sys
+import pygame
+import requests
+
+pt = ''
+flag = False
+
+
+def createMap(x, y, z, l):
+    global pt
+    slide = f"http://static-maps.yandex.ru/1.x/?ll={x},{y}&z={z}&l={l}"
+    api_server = "http://static-maps.yandex.ru/1.x/"
+
+    params = {
+        "ll": ",".join([str(x), str(y)]),
+        "l": l,
+        'z': z,
+    }
+    if pt:
+        params['pt'] = pt
+
+    response = requests.get(api_server, params=params)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(slide)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_file = 'Data/maps/map.png'
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return 'Data/maps/map.png'
+
+
+def find_toponim(adress):
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={adress}&format=json"
+
+    global pt, flag
+    # Выполняем запрос.
+    response = requests.get(geocoder_request)
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+
+        # Получаем первый топоним из ответа геокодера.
+        # Согласно описанию ответа, он находится по следующему пути:
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        # Полный адрес топонима:
+        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+        if flag:
+            try:
+                toponym_address += ', ' + toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+            except BaseException:
+                toponym_address += ', почтовый индекс не найден'
+        # Координаты центра топонима:
+        toponym_coodrinates = toponym["Point"]["pos"]
+        # Печатаем извлечённые из ответа поля:
+        x, y = toponym_coodrinates.split()
+        pt = ",".join([x, y, 'org'])
+        return toponym_coodrinates, toponym_address
+    else:
+        print("Ошибка выполнения запроса:")
+        print(geocoder_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+
+
+def click_map(x, y, delta, org):
+    if org:
+        geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&type=biz&geocode={','.join([str(x), str(y)])}&spn=0.0005,0.0005&format=json"
+    else:
+        geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={','.join([str(x), str(y)])}&spn={delta},{delta}&format=json"
+
+    global pt, flag
+    # Выполняем запрос.
+    response = requests.get(geocoder_request)
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+
+        # Получаем первый топоним из ответа геокодера.
+        # Согласно описанию ответа, он находится по следующему пути:
+        toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+        # Полный адрес топонима:
+        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+        if flag:
+            try:
+                toponym_address += ', ' + toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["postal_code"]
+            except BaseException:
+                toponym_address += ', почтовый индекс не найден'
+        # Координаты центра топонима:
+        toponym_coodrinates = toponym["Point"]["pos"]
+        # Печатаем извлечённые из ответа поля:
+        x, y = toponym_coodrinates.split()
+        pt = ",".join([x, y, 'org'])
+        return toponym_address
+    else:
+        return ''
